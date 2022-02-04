1451075fab17c072c161376fcff921dbfaef59d4
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-04 20:25:23 -0300

    exercicios de fixacao

---
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/primeiros-passos/scripts.js
@@ -0,0 +1,21 @@
+// Variáveis
+
+// 01. Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
+const myName = 'Marcel';
+
+// 02. Crie uma constante chamada birthCity e atribua a ela a sua cidade natal.
+const birthCity = 'Natal';
+
+// 03. Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
+const birthYear = 1993;
+
+// 04. Utilize o console.log() para imprimir as constantes e variáveis que você criou.
+console.log(myName, birthCity, birthYear);
+
+// 05. Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+birthYear = 2030;
+console.log(birthYear); // erro
+
+// 06. Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro? 🤔
+birthCity = Porto;
+console.log(birthCity); // erro
