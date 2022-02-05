4e799802d63e5fabb85457d23b7d7e46e7e27e9e
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-05 19:02:40 -0300

    exercicio questao 10

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/scripts.js
@@ -171,3 +171,24 @@ if ((a % 2 == 1) || (b % 2 == 1) || (i % 2 == 1)) {
 
 else
     console.log(false);
+
+//--------------------------------------------------------------------------------------------------------------------
+console.log(d);
+// 10. Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda.A partir dos valores, calcule quanto de lucro(valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+const k = 2;
+const l = 5;
+
+// Atente que, sobre o custo do produto, incide um imposto de 20 %.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+const valorCustoTotal = k + (20 / 100);
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20 % também faz parte do valor de custo.
+// lucro = valorVenda - valorCustoTotal(lucro de um produto)
+const lucro = (5 - valorCustoTotal) * 1000;
+
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+if (k < 0 || l < 0) {
+    console.log("Erro");
+}
+
+console.log(lucro);
