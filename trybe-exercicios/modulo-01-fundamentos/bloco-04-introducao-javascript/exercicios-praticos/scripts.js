ea31b0d4be805382838cc3af2f7277afdf4dd6cd
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-05 14:28:38 -0300

    exercicioquestao 01

---
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/scripts.js
@@ -0,0 +1,30 @@
+// --------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------- JAVASCRIPT - PRIMEIROS PASSOS ------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------ EXERCÍCIOS PRÁTICOS -----------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
+
+// 01. Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição (a + b)
+const a = 3;
+const b = 2;
+
+const soma = a + b;
+console.log(soma);
+
+// Subtração (a - b)
+const sub = a - b;
+console.log(sub);
+
+// Multiplicação (a * b)
+const mult = a * b;
+console.log(mult);
+
+// Divisão (a / b)
+const div = a / b;
+console.log(div);
+
+// Módulo (a % b)
+const mod = a % b;
+console.log(mod);
