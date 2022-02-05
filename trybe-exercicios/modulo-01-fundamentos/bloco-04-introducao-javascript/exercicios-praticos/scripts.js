314feb444bb3d7422878f422f6e9de1b12515b52
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-05 20:48:38 -0300

    exercicio questao 11

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/scripts.js
@@ -192,3 +192,69 @@ if (k < 0 || l < 0) {
 }
 
 console.log(lucro);
+
+//--------------------------------------------------------------------------------------------------------------------
+console.log(d);
+
+// 11. Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR.Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+
+const salarioBruto = 10000;
+
+// INSS(Instituto Nacional do Seguro Social)
+// Salário bruto até R$ 1.556,94: alíquota de 8 %
+// IR(Imposto de Renda)
+// Até R$ 1.903,98: isento de imposto de renda
+if (salarioBruto <= 1556.94) {
+
+    const salarioLiquido = salarioBruto - (salarioBruto * 0.08);
+
+    console.log(salarioLiquido);
+}
+
+// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9 % 
+// De R$ 1.903,99 a 2.826,65: alíquota de 7,5 % e parcela de R$ 142,80 a deduzir do imposto
+if ((salarioBruto >= 1556.95) && (salarioBruto <= 2594.92)) {
+
+    const salarioBase = salarioBruto - (salarioBruto * 0.09)
+
+    if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+        const salarioLiquido = (salarioBase - (salarioBase * 0.075));
+        console.log(salarioLiquido);
+    }
+}
+
+// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11 %
+// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15 % e parcela de R$ 354,80 a deduzir do imposto
+// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5 % e parcela de R$ 636,13 a deduzir do imposto
+if ((salarioBruto >= 2594.93) && (salarioBruto <= 5189.82)) {
+
+    const salarioBase = salarioBruto - (salarioBruto * 0.11);
+
+    if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+        const salarioLiquido = salarioBase - (salarioBase * 0.075 - 142.80);
+        console.log(salarioLiquido.toFixed(2));
+    }
+    if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+        const salarioLiquido = salarioBase - (salarioBase * 0.15 - 354.80);
+        console.log(salarioLiquido.toFixed(2));
+    }
+    if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        const salarioLiquido = salarioBase - (salarioBase * 0.225 - 636.13);
+        console.log(salarioLiquido.toFixed(2));
+    }
+}
+
+// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+// Acima de R$ 4.664,68: alíquota de 27,5 % e parcela de R$ 869,36 a deduzir do imposto.
+if (salarioBruto > 5189.82) {
+    const salarioBase = salarioBruto - 570.88;
+
+    if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        const salarioLiquido = salarioBase - (salarioBase * 0.225 - 636.13);
+        console.log(salarioLiquido.toFixed(2));
+    }
+    if (salarioBase > 4664.68) {
+        const salarioLiquido = salarioBase - (salarioBase * 0.275 - 869.36);
+        console.log(salarioLiquido.toFixed(2));
+    }
+}
