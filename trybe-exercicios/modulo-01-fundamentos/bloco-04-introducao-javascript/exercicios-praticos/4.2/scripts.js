59ca11096dfca465fbe77119f1c633f254f8af2c
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-06 20:30:01 -0300

    adiciona questao 03

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -6,13 +6,23 @@
 
 // 01. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (const number of numbers) {
-    console.log(number);
-}
+// for (const number of numbers) {
+//     console.log(number);
+// }
 
-// 02. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+// // 02. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     soma += numbers[i];
+// }
+// console.log(soma);
+
+// 03. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
@@ -20,5 +30,6 @@ let soma = 0;
 for (let i = 0; i < numbers.length; i++) {
     soma += numbers[i];
 }
-console.log(soma);
 
+let media = soma / numbers.length;
+console.log(media);
