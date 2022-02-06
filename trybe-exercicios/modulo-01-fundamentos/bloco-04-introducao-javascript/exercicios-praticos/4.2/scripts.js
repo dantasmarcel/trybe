2d92063699e98317e6f8e3795955a659c7de1736
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-06 20:33:42 -0300

    adiciona questao 04

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -33,3 +33,12 @@ for (let i = 0; i < numbers.length; i++) {
 
 let media = soma / numbers.length;
 console.log(media);
+
+// 04. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+if (media > 20) {
+    console.log('valor maior que 20');
+}
+else
+    console.log('valor menor ou igual a 20');
+
