58bcee2cdc2bdad12a4b25f4d4ad016457da8ce4
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-06 20:23:41 -0300

    adiciona questao 02

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -12,3 +12,13 @@ for (const number of numbers) {
     console.log(number);
 }
 
+// 02. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    soma += numbers[i];
+}
+console.log(soma);
+
