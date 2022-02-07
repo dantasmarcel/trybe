ed697b5a3029b14d2bd6462b0059894854a2beaf
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-07 19:30:36 -0300

    adiciona questao 09

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -97,3 +97,14 @@ for (let i = 1; i <= 25; i += 1) {
 }
 
 console.log(array);
+
+// 09. Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+
+let array = [];
+
+for (let i = 1; i <= 25; i += 1) {
+    array.push(i / 2);
+}
+
+console.log(array);
