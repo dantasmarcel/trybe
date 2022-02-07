673366e2ffb7709060b339fb1bbdaf2ac86c7162
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-06 22:36:57 -0300

    adiciona questao 06

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -61,3 +61,15 @@ let maior = numbers.reduce(function (a, b) {
 
 });
 console.log(maior);
+
+// 06. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let impar = 0;
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 1) {
+        impar += 1;
+    }
+}
+console.log(impar);
