582d62890296005a2d70981bf18b7cecec60be8a
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-07 19:27:07 -0300

    adiciona questao 08

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -87,3 +87,13 @@ for (let i = 0; i < numbers.length; i++) {
     }
 }
 console.log(menor);
+
+// 08. Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let array = [];
+
+for (let i = 1; i <= 25; i += 1) {
+    array.push(i);
+}
+
+console.log(array);
