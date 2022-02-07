daedadd3bb943204b592df440c25a7372755edfc
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-07 19:37:12 -0300

    adiciona exercicio bonus 01

--- trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-04-introducao-javascript/exercicios-praticos/4.2/scripts.js
@@ -108,3 +108,25 @@ for (let i = 1; i <= 25; i += 1) {
 }
 
 console.log(array);
+
+// --------------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------- EXERCICIOS-BÔNUS -----------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------------
+
+
+// 01. Ordene o array numbers em ordem crescente e imprima seus valores;
+
+
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < array.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (array[index] < array[secondIndex]) {
+            let position = array[index];
+            array[index] = array[secondIndex];
+            array[secondIndex] = position;
+        }
+    }
+}
+
+console.log(array);
