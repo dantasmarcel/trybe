88cc8815f798d7d05bbd18e03e28c43c6b1e4b27
Marcel Dantas <allefmarcel.dev@gmail.com>
2022-02-18 13:38:44 -0300

    alterando o titulo da pagina

--- trybe-exercicios/modulo-01-fundamentos/bloco-05-dom-eventos-web-storage/5.1/primeiros-passos/script.js
+++ trybe-exercicios/modulo-01-fundamentos/bloco-05-dom-eventos-web-storage/5.1/primeiros-passos/script.js
@@ -1,2 +1,7 @@
 const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
+
+// 01. Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
+
+const title = document.getElementById("page-title");
+title.innerHTML = 'Velozes e Furiosos';
